54aacd4edfaf8cc47430fa22731b5fc5ff9c24f8
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2021-10-12 19:12:21 +0200

    add dsfr 1.1

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("./src/css");
+    eleventyConfig.addPassthroughCopy("./src/js");    
+    eleventyConfig.addPassthroughCopy("./src/fonts");    
+    eleventyConfig.addPassthroughCopy("./src/favicon");    
     return {
       dir: {
         input: "src",

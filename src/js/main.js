58fea95181d656c589d16e9f9ac519abc4b449c8
Yaacov <yaacov@goodimpact.studio>
2022-02-08 01:03:03 +0200

    Add first filter to display/hide all criteria tests
    Note: a disturbing scroll to the end of the page happens on hide

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,26 @@
-export default function () {
-	// TODO: add scripts
+// Handle the 3 filters in criteria page
+const filtresEl = document.querySelector("#filtres-criteres");
+if (filtresEl) {
+	// Everything happens under "filtres-criteres" element
+	filtresEl.addEventListener("change", function (e) {
+		const curEl = e.target;
+		const filter = curEl.dataset.filter;
+		const checked = curEl.checked;
+		switch (filter) {
+			case "all":
+				const criteriaEls =
+					filtresEl.parentElement.getElementsByTagName("button");
+				Array.from(criteriaEls).forEach(function (el) {
+					el.setAttribute("aria-expanded", checked);
+				});
+				break;
+			case "A":
+				break;
+			case "AA":
+				break;
+			default:
+				// should never happen
+				break;
+		}
+	});
 }

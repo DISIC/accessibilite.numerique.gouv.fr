83ff650fa158d91398d4edbfedf1133bcf3b5d61
Quentin Bellanger <hello@quentin-bellanger.com>
2022-06-09 18:12:37 +0200

    add missing href attribute to links

--- scripts/generate-glossary.js
+++ scripts/generate-glossary.js
@@ -41,8 +41,8 @@ async function generateGlossary() {
       const url = `${baseUrl}/methode/criteres-et-tests/`
 
       cleanedContent = content
-        .replace(critRegex, `<a ${url}#$<id>">critère $<id></a>`)
-        .replace(testRegex, `<a ${url}#$<id>">test $<id></a>`)
+        .replace(critRegex, `<a href="${url}#$<id>">critère $<id></a>`)
+        .replace(testRegex, `<a href="${url}#$<id>">test $<id></a>`)
 
       // Push to JSON data
       jsonData.glossary.push({ title, body: cleanedContent })

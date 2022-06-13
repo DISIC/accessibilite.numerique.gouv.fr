9140390d0936ccf83d1b77645ac20a269994c523
Quentin Bellanger <hello@quentin-bellanger.com>
2022-06-13 10:54:48 +0200

    format scripts

--- scripts/generate-criteria.js
+++ scripts/generate-criteria.js
@@ -1,26 +1,26 @@
-const fs = require('fs').promises;
-const fm = require('front-matter')
+const fs = require("fs").promises;
+const fm = require("front-matter");
 
-const themes = require('../src/_data/themes.json')
+const themes = require("../src/_data/themes.json");
 
 /*
   TODO:
     - add npm command
 */
 
-const CRITERIA_SOURCE = './src/rgaa/criteres'
-const CRITERIA_DESTINATION = './RGAA/4.1/criteres.json'
+const CRITERIA_SOURCE = "./src/rgaa/criteres";
+const CRITERIA_DESTINATION = "./RGAA/4.1/criteres.json";
 
 /**
  * Get criterion title
  * @param {string} path
  * @returns {Promise<string>} title attribute
  */
- async function parseTitle(path) {
-  const data = await fs.readFile(path, "utf-8");
-  const result = await fm(data)
+async function parseTitle(path) {
+	const data = await fs.readFile(path, "utf-8");
+	const result = await fm(data);
 
-  return result.attributes.title
+	return result.attributes.title;
 }
 
 /**
@@ -28,18 +28,18 @@ const CRITERIA_DESTINATION = './RGAA/4.1/criteres.json'
  * @param {string} path
  * @returns {Promise<object>}
  */
- async function parseReferences(path) {
-  const data = await fs.readFile(path, "utf-8");
-  const result = await fm(data)
+async function parseReferences(path) {
+	const data = await fs.readFile(path, "utf-8");
+	const result = await fm(data);
 
-  const wcag = result.attributes.WCAG.map(w => {
-    return `${w.successCriterion} ${w.title} (${w.level})`
-  })
+	const wcag = result.attributes.WCAG.map((w) => {
+		return `${w.successCriterion} ${w.title} (${w.level})`;
+	});
 
-  return {
-    wcag,
-    techniques: result.attributes.Techniques
-  }
+	return {
+		wcag,
+		techniques: result.attributes.Techniques,
+	};
 }
 
 /**
@@ -47,20 +47,20 @@ const CRITERIA_DESTINATION = './RGAA/4.1/criteres.json'
  * @param {string} folderPath
  * @returns {Promise<array|string>}
  */
- async function parseTests(folderPath) {
-  const tests = {}
+async function parseTests(folderPath) {
+	const tests = {};
 
-  const folders = await fs.readdir(folderPath);
-  folders.forEach(async (_, i) => {
-    const data = await fs.readFile(`${folderPath}/${i + 1}.md`, "utf-8");
-    const result = fm(data)
+	const folders = await fs.readdir(folderPath);
+	folders.forEach(async (_, i) => {
+		const data = await fs.readFile(`${folderPath}/${i + 1}.md`, "utf-8");
+		const result = fm(data);
 
-    tests[i + 1] = result.attributes.steps
-      ? [result.attributes.title, ...result.attributes.steps]
-      : result.attributes.title
-  })
+		tests[i + 1] = result.attributes.steps
+			? [result.attributes.title, ...result.attributes.steps]
+			: result.attributes.title;
+	});
 
-  return tests
+	return tests;
 }
 
 /**
@@ -69,19 +69,19 @@ const CRITERIA_DESTINATION = './RGAA/4.1/criteres.json'
  * @returns {string[]}
  */
 function formatPCAndTN(str) {
-  const critRegex = /\{% crit (?<id>\d{1,2}.\d{1,2}) %\}/g // {% crit 12.10 %}
-  const testRegex = /\{% test '(?<id>\d{1,2}.\d{1,2}.\d{1,2})' %\}/g // {% test 2.10.3 %}
-  const baseUrl = 'https://accessibilite.numerique.gouv.fr'
-  const url = `${baseUrl}/methode/criteres-et-tests/`
+	const critRegex = /\{% crit (?<id>\d{1,2}.\d{1,2}) %\}/g; // {% crit 12.10 %}
+	const testRegex = /\{% test '(?<id>\d{1,2}.\d{1,2}.\d{1,2})' %\}/g; // {% test 2.10.3 %}
+	const baseUrl = "https://accessibilite.numerique.gouv.fr";
+	const url = `${baseUrl}/methode/criteres-et-tests/`;
 
-  return str
-    .split('\n')
-    .filter(Boolean)
-    .map(s => {
-      return s
-        .replace(critRegex, `<a href="${url}#$<id>">critère $<id></a>`)
-        .replace(testRegex, `<a href="${url}#$<id>">test $<id></a>`)
-    })
+	return str
+		.split("\n")
+		.filter(Boolean)
+		.map((s) => {
+			return s
+				.replace(critRegex, `<a href="${url}#$<id>">critère $<id></a>`)
+				.replace(testRegex, `<a href="${url}#$<id>">test $<id></a>`);
+		});
 }
 
 /**
@@ -90,21 +90,21 @@ function formatPCAndTN(str) {
  * @returns {array}
  */
 function handlePCAndTNSubItems(lines) {
-  const result = []
+	const result = [];
 
-  lines.forEach(line => {
-    if (line.startsWith('- ')) {
-      if (typeof result.at(-1) === 'object') {
-        result.at(-1).ul.push(line)
-      } else {
-        result.push({ ul: [line] })
-      }
-    } else {
-      result.push(line)
-    }
-  })
+	lines.forEach((line) => {
+		if (line.startsWith("- ")) {
+			if (typeof result.at(-1) === "object") {
+				result.at(-1).ul.push(line);
+			} else {
+				result.push({ ul: [line] });
+			}
+		} else {
+			result.push(line);
+		}
+	});
 
-  return result
+	return result;
 }
 
 /**
@@ -113,26 +113,39 @@ function handlePCAndTNSubItems(lines) {
  * @returns {Promise<object>}
  */
 async function parsePCAndTN(path) {
-  const data = await fs.readFile(path, "utf-8");
-  const result = await fm(data)
+	const data = await fs.readFile(path, "utf-8");
+	const result = await fm(data);
 
-  const parts = result.body.split(/(#### .*)/g).map(el => el.trim()).filter(Boolean)
-  const hasTN = parts.includes('#### Notes techniques') || parts.includes('#### Note technique')
-  const hasPC = parts.includes('#### Cas particuliers')
+	const parts = result.body
+		.split(/(#### .*)/g)
+		.map((el) => el.trim())
+		.filter(Boolean);
+	const hasTN =
+		parts.includes("#### Notes techniques") ||
+		parts.includes("#### Note technique");
+	const hasPC = parts.includes("#### Cas particuliers");
 
-  if (hasTN && hasPC) {
-    const TNIndex = parts.findIndex(p => p.match(/#### Notes? techniques?/))
-    return {
-      particularCases: handlePCAndTNSubItems(formatPCAndTN(parts.slice(1, TNIndex)[0])),
-      technicalNote: handlePCAndTNSubItems(formatPCAndTN(parts.slice(TNIndex + 1)[0]))
-    }
-  } else if (hasTN) {
-    return { technicalNote: handlePCAndTNSubItems(formatPCAndTN(parts.slice(1)[0]) )}
-  } else if (hasPC) {
-    return { particularCases: handlePCAndTNSubItems(formatPCAndTN(parts.slice(1)[0]) )}
-  }
+	if (hasTN && hasPC) {
+		const TNIndex = parts.findIndex((p) => p.match(/#### Notes? techniques?/));
+		return {
+			particularCases: handlePCAndTNSubItems(
+				formatPCAndTN(parts.slice(1, TNIndex)[0])
+			),
+			technicalNote: handlePCAndTNSubItems(
+				formatPCAndTN(parts.slice(TNIndex + 1)[0])
+			),
+		};
+	} else if (hasTN) {
+		return {
+			technicalNote: handlePCAndTNSubItems(formatPCAndTN(parts.slice(1)[0])),
+		};
+	} else if (hasPC) {
+		return {
+			particularCases: handlePCAndTNSubItems(formatPCAndTN(parts.slice(1)[0])),
+		};
+	}
 
-  return {}
+	return {};
 }
 
 /**
@@ -141,10 +154,10 @@ async function parsePCAndTN(path) {
  * @returns string
  */
 function transformGlossaryLinks(str) {
-  const regex = /\[(?<label>.*?)\]\(#(?<slug>.*?)\)/g // bla [label](#slug) bla
-  const baseUrl = 'https://accessibilite.numerique.gouv.fr'
-  const url = `${baseUrl}/methode/glossaire/`
-  return str.replace(regex, `<a href="${url}#$<slug>">$<label></a>`)
+	const regex = /\[(?<label>.*?)\]\(#(?<slug>.*?)\)/g; // bla [label](#slug) bla
+	const baseUrl = "https://accessibilite.numerique.gouv.fr";
+	const url = `${baseUrl}/methode/glossaire/`;
+	return str.replace(regex, `<a href="${url}#$<slug>">$<label></a>`);
 }
 
 /**
@@ -152,50 +165,54 @@ function transformGlossaryLinks(str) {
  * from `src/rgaa/criteres` grouped by topics.
  */
 async function generateCriteria() {
-  try {
-    // Init JSON data
-    let jsonData = { wcag: { version: 2.1 }, topics: [] }
-    const criteria = []
+	try {
+		// Init JSON data
+		let jsonData = { wcag: { version: 2.1 }, topics: [] };
+		const criteria = [];
 
-    const folders = await fs.readdir(CRITERIA_SOURCE);
-    for (const folder of folders) {
-      // Build all criterion properties
-      const criterionObject = {
-        criterium: {
-          number: folder,
-          title: await parseTitle(`${CRITERIA_SOURCE}/${folder}/index.md`),
-          tests: await parseTests(`${CRITERIA_SOURCE}/${folder}/tests`),
-          ...(await parsePCAndTN(`${CRITERIA_SOURCE}/${folder}/annexe.md`))
-        },
-        references: await parseReferences(`${CRITERIA_SOURCE}/${folder}/annexe.md`)
-      }
+		const folders = await fs.readdir(CRITERIA_SOURCE);
+		for (const folder of folders) {
+			// Build all criterion properties
+			const criterionObject = {
+				criterium: {
+					number: folder,
+					title: await parseTitle(`${CRITERIA_SOURCE}/${folder}/index.md`),
+					tests: await parseTests(`${CRITERIA_SOURCE}/${folder}/tests`),
+					...(await parsePCAndTN(`${CRITERIA_SOURCE}/${folder}/annexe.md`)),
+				},
+				references: await parseReferences(
+					`${CRITERIA_SOURCE}/${folder}/annexe.md`
+				),
+			};
 
-      // Push to JSON data
-      criteria.push(criterionObject)
-    }
+			// Push to JSON data
+			criteria.push(criterionObject);
+		}
 
-    // Organize criteria by topic
-    const topics = Object.entries(themes).map(t => {
-      const topicNumber = t[0]
-      return {
-        topic: t[1].title,
-        number: topicNumber,
-        criteria: criteria.filter(c => {
-          return c.criterium.number.split('.')[0] === topicNumber
-        })
-      }
-    })
+		// Organize criteria by topic
+		const topics = Object.entries(themes).map((t) => {
+			const topicNumber = t[0];
+			return {
+				topic: t[1].title,
+				number: topicNumber,
+				criteria: criteria.filter((c) => {
+					return c.criterium.number.split(".")[0] === topicNumber;
+				}),
+			};
+		});
 
-    jsonData.topics = topics
+		jsonData.topics = topics;
 
-    // Create JSON file
-    let data = JSON.stringify(jsonData, null, 2)
-    await fs.writeFile(CRITERIA_DESTINATION, data);
+		// Create JSON file
+		let data = JSON.stringify(jsonData, null, 2);
+		await fs.writeFile(CRITERIA_DESTINATION, data);
 
-    console.log(`✅ Criteria list successfully generated.`);
-  } catch(error) {
-    console.error(`❌ An error occured while generating criteria list: ${error}`);
-  }
+		console.log(`✅ Criteria list successfully generated.`);
+	} catch (error) {
+		console.error(
+			`❌ An error occured while generating criteria list: ${error}`
+		);
+	}
 }
 
-generateCriteria()
+generateCriteria();
